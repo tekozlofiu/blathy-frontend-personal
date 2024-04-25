79685d8514ec9afb68518e342164c6849caf951c
tekozlofiu <tekozlofiu@gmail.com>
2024-04-25 17:25:26 +0200

    Add axios

--- DrinkFrontend/src/views/HomeView.vue
+++ DrinkFrontend/src/views/HomeView.vue
@@ -1,11 +1,42 @@
 <script>
 export default {
-  name: "HomeView"
+  name: "HomeView",
+
+  data() {
+    return [
+
+    ]
+  },
+
+  props: {
+
+  },
 }
 </script>
 
 <template>
-  <h1>Helló, mi?!</h1>
+
+  <main class="container">
+
+    <h1>Italok</h1>
+
+    <hr>
+
+    <table class="table">
+      <thead>
+      <tr>
+        <th>Név</th>
+        <th>Ár</th>
+        <th>Akciós</th>
+        <th>Művelet</th>
+      </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </table>
+  </main>
+
 </template>
 
 <style scoped>
